f39858b299bb3fbb202f378301c974ca2f474efa
Danie José Dias Teixeira <daniel.jd.teixeira@gmail.com>
2019-11-02 19:36:52 -0300

    Criado layout dos itens e da tela de listagem de repositórios

--- app/src/main/java/com/danjdt/githubjavarepos/ui/repositories/RepositoriesActivity.kt
+++ app/src/main/java/com/danjdt/githubjavarepos/ui/repositories/RepositoriesActivity.kt
@@ -1,9 +1,17 @@
 package com.danjdt.githubjavarepos.ui.repositories
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.danjdt.githubjavarepos.R
 
 /**
  *  @autor danieljdt
  *  @date 2019-11-01
  **/
-class RepositoriesActivity : AppCompatActivity()
+class RepositoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repositories)
+    }
+}
