6658e7ea6608b3fe238be7b5e810c75026095aff
Danie José Dias Teixeira <daniel.jd.teixeira@gmail.com>
2019-11-02 01:57:02 -0300

    Implementado GithubApi e Reponse Data class

---
+++ data/src/main/java/com/danjdt/data/network/GithubApi.kt
@@ -0,0 +1,22 @@
+package com.danjdt.data.network
+
+import com.danjdt.data.response.PullRequestResponse
+import com.danjdt.data.response.RepositoriesReponse
+import io.reactivex.Observable
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.GET
+
+/**
+ *  @autor danieljdt
+ *  @date 2019-11-02
+ **/
+interface GithubApi {
+
+    @GET("search/repositories?q=language:Java&sort=stars&page={page}")
+    fun fetchJavaRepositories(@Field("page") page: Int): Observable<RepositoriesReponse>
+
+    @GET("repos/{owner}/{repository}/pulls")
+    fun fetchPullRequests(@Field("owner") owner: String, @Field("repository") repository: String): Call<PullRequestResponse>
+
+}
