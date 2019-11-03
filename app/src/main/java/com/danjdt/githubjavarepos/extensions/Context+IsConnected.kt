fc871f65924c21b1de4b666a4dde5830e6b5f783
danieljdt <ngy3uo45>
2019-11-03 11:00:32 -0300

    Implementado view de erro

---
+++ app/src/main/java/com/danjdt/githubjavarepos/extensions/Context+IsConnected.kt
@@ -0,0 +1,15 @@
+package com.danjdt.githubjavarepos.extensions
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+/**
+ *  @autor danieljdt
+ *  @date 2019-11-03
+ **/
+
+val Context.isConnected: Boolean
+    get() {
+        return (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+            .activeNetworkInfo?.isConnected == true
+    }
