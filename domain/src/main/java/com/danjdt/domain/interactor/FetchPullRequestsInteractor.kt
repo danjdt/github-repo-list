e471d3852a333c62928a5efb56f29014d93af456
danieljdt <ngy3uo45>
2019-11-03 14:26:10 -0300

    'FetchPullRequestsInteractor' agora tem retorno não optional

--- domain/src/main/java/com/danjdt/domain/interactor/FetchPullRequestsInteractor.kt
+++ domain/src/main/java/com/danjdt/domain/interactor/FetchPullRequestsInteractor.kt
@@ -3,12 +3,11 @@ package com.danjdt.domain.interactor
 import com.danjdt.domain.entity.PullRequest
 import com.danjdt.domain.interactor.FetchPullRequestsInteractor.Params
 
-
 /**
  *  @autor danieljdt
  *  @date 2019-11-02
  **/
-interface FetchPullRequestsInteractor : Interactor<List<PullRequest>?, Params> {
+interface FetchPullRequestsInteractor : Interactor<List<PullRequest>, Params> {
 
     data class Params(val owner: String, val repository: String, val page: Int)
 }
